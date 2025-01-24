def473e641acbc4f100459a3542d29adac865aef
Kauan de Oliveira Carvalho <kauan.carvalho@corregedoria.df>
2025-01-24 10:47:57 -0300

    projeto user

---
+++ Crud/src/app/user/get-user-by-id/get-user-by-id.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GetUserByIDComponent } from './get-user-by-id.component';
+
+describe('GetUserByIDComponent', () => {
+  let component: GetUserByIDComponent;
+  let fixture: ComponentFixture<GetUserByIDComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [GetUserByIDComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GetUserByIDComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Crud/src/app/user/load-user/load-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-user',
+  standalone: false,
+  
+  templateUrl: './load-user.component.html',
+  styleUrl: './load-user.component.css'
+})
+export class LoadUserComponent {
+
+}
